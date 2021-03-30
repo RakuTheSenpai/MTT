f7775fd3b6f9d479ce716941ad11ce5328722bca
Raku <raul.r.h.rosas@gmail.com>
2021-03-30 15:55:51 -0700

    Mostly done

--- MTT.cpp
+++ MTT.cpp
@@ -3,28 +3,28 @@
 
 void show_process(cv::Mat &frame, const cv::Ptr<cv::BackgroundSubtractorKNN>&background_sub){
     if(frame.empty()) return;
-    cv::Mat gray, thresh;
-    int height = frame.size().height;
-    int width = frame.size(). width;
-    //Create black empty Matrix
-    cv::Mat complete{cv::Size{width, height}, frame.type()};
+    cv::Mat masked;
     //Resize frame to half
-    int nwidth = width/2;
-    int nheight = height/2;
-    cv::resize(frame, frame, cv::Size{nwidth, nheight});
-    //Color gray then turn back to BGR channel to permit copy (cv doesn't allow images with different channels to be copied)
-    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-    //Perform threshold
-    cv::threshold(gray, thresh, 127, 255, cv::THRESH_BINARY_INV);
     //Image substraction
-    background_sub->apply(thresh, thresh);
-    cv::cvtColor(thresh, thresh, cv::COLOR_GRAY2BGR);
-    //Copy images into new Mat 
-    cv::cvtColor(gray, gray, cv::COLOR_GRAY2BGR);
-    frame.copyTo(complete(cv::Rect(0,0,nwidth, nheight)));
-    gray.copyTo(complete(cv::Rect(nwidth, 0, nwidth, nheight)));
-    thresh.copyTo(complete(cv::Rect(0, nheight, nwidth, nheight)));
-    cv::imshow("Frame", complete);
+    background_sub->apply(frame, masked);
+    cv::imshow("First", masked);
+    cv::threshold(masked, masked, 254, 255, cv::THRESH_BINARY);
+    //Contour finding
+    std::vector<std::vector<cv::Point>>contours;
+    std::vector<cv::Vec4i>hierarchy;
+    cv::findContours(masked, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+    //Draw Contours
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+        cv::Scalar color = cv::Scalar( 0, 255, 0 );
+        int area = cv::contourArea(contours[i]);
+        if(area > 100){
+            auto bound = cv::boundingRect(contours[i]);
+            cv::rectangle(frame,bound,color);
+        }
+    }
+    cv::imshow("Threshold", masked);
+    cv::imshow("Frame",frame);
 }
 int main(int argc, char** argv )
 {
@@ -40,10 +40,10 @@ int main(int argc, char** argv )
     }
     auto background_sub = cv::createBackgroundSubtractorKNN();
     while(1){
-        cv::Mat frame, gray, thresh;
+        cv::Mat frame;
         cap >> frame;
         show_process(frame, background_sub);
-        if(cv::waitKey(30)>= 0)break;
+        if(cv::waitKey(30)>= 0 || frame.empty())break;
     }
     return 0;
 }
