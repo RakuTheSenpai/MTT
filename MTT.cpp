a5bc1d2331344fd89e6d2cfdbb6de9ba3cfd1ef0
Raku <raul.r.h.rosas@gmail.com>
2021-03-30 15:58:01 -0700

    Mostly done

--- MTT.cpp
+++ MTT.cpp
@@ -1,13 +1,12 @@
 #include <opencv2/opencv.hpp> 
 #include <iostream>
 
-void show_process(cv::Mat &frame, const cv::Ptr<cv::BackgroundSubtractorKNN>&background_sub){
+void process(cv::Mat &frame, const cv::Ptr<cv::BackgroundSubtractorKNN>&background_sub){
     if(frame.empty()) return;
     cv::Mat masked;
     //Resize frame to half
     //Image substraction
     background_sub->apply(frame, masked);
-    cv::imshow("First", masked);
     cv::threshold(masked, masked, 254, 255, cv::THRESH_BINARY);
     //Contour finding
     std::vector<std::vector<cv::Point>>contours;
@@ -18,13 +17,12 @@ void show_process(cv::Mat &frame, const cv::Ptr<cv::BackgroundSubtractorKNN>&bac
     {
         cv::Scalar color = cv::Scalar( 0, 255, 0 );
         int area = cv::contourArea(contours[i]);
-        if(area > 100){
+        if(area > 500){
             auto bound = cv::boundingRect(contours[i]);
             cv::rectangle(frame,bound,color);
         }
     }
-    cv::imshow("Threshold", masked);
-    cv::imshow("Frame",frame);
+    cv::imshow("MTT",frame);
 }
 int main(int argc, char** argv )
 {
@@ -42,7 +40,7 @@ int main(int argc, char** argv )
     while(1){
         cv::Mat frame;
         cap >> frame;
-        show_process(frame, background_sub);
+        process(frame, background_sub);
         if(cv::waitKey(30)>= 0 || frame.empty())break;
     }
     return 0;
