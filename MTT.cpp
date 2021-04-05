148098b472d5c7dd4672bbbfdb1e6dadc54177c6
Raku <raul.r.h.rosas@gmail.com>
2021-04-05 14:37:18 -0700

    Recognition done, missing tracking

--- MTT.cpp
+++ MTT.cpp
@@ -1,28 +1,25 @@
 #include <opencv2/opencv.hpp> 
+#include <opencv2/tracking.hpp>
 #include <iostream>
 
 void process(cv::Mat &frame, const cv::Ptr<cv::BackgroundSubtractorKNN>&background_sub){
     if(frame.empty()) return;
     cv::Mat masked;
-    //Resize frame to half
     //Image substraction
     background_sub->apply(frame, masked);
     cv::threshold(masked, masked, 254, 255, cv::THRESH_BINARY);
-    //Contour finding
     std::vector<std::vector<cv::Point>>contours;
-    std::vector<cv::Vec4i>hierarchy;
-    cv::findContours(masked, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-    //Draw Contours
-    for( size_t i = 0; i< contours.size(); i++ )
-    {
-        cv::Scalar color = cv::Scalar( 0, 255, 0 );
-        int area = cv::contourArea(contours[i]);
-        if(area > 500){
-            auto bound = cv::boundingRect(contours[i]);
-            cv::rectangle(frame,bound,color);
+    cv::findContours(masked, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    std::vector<std::vector<cv::Point>>convex_hulls(contours.size());
+    for(int i = 0; i < contours.size(); ++i) cv::convexHull(contours[i], convex_hulls[i]);
+    for(auto &convex_hull:convex_hulls){
+        auto rect = cv::boundingRect(convex_hull);
+        double diagonal = std::sqrt(std::pow(rect.width, 2) + std::pow(rect.height, 2));
+        if(rect.area() > 100 && rect.width > 15 && rect.height > 20 && diagonal > 30){
+            cv::rectangle(frame, rect, cv::Scalar(0,255,0), 2);
         }
     }
-    cv::imshow("MTT",frame);
+    cv::imshow("MTT", frame);
 }
 int main(int argc, char** argv )
 {
@@ -36,9 +33,11 @@ int main(int argc, char** argv )
         std::cout << "Couldn't open VideoCapture\n";
         return -1;
     }
-    auto background_sub = cv::createBackgroundSubtractorKNN();
-    while(1){
-        cv::Mat frame;
+    auto background_sub = cv::createBackgroundSubtractorKNN(100, 450);
+    cv::Ptr<cv::Tracker>tracker;
+    tracker = cv::TrackerKCF::create();
+    cv::Mat frame;
+    while(cap.isOpened()){
         cap >> frame;
         process(frame, background_sub);
         if(cv::waitKey(30)>= 0 || frame.empty())break;
